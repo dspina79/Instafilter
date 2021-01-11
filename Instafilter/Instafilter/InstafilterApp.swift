b52efaa738b5db91fb2f1be01b7ae71e4e42d3f9
Dave Spina <dspina79@gmail.com>
2021-01-11 09:49:43 -0500

    Initial application creation

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Dave Spina on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
