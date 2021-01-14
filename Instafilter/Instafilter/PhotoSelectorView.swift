0f76c2f6c3518588141069776de97082d045ba20
Dave Spina <dspina79@gmail.com>
2021-01-13 23:51:43 -0500

    Added example code to save image to the library

--- Instafilter/Instafilter/PhotoSelectorView.swift
+++ Instafilter/Instafilter/PhotoSelectorView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save Complete")
+    }
+}
+
 struct PhotoSelectorView: View {
     @State private var image: Image?
     @State private var showImagePicker = false
@@ -29,6 +39,25 @@ struct PhotoSelectorView: View {
     func loadImage() {
         guard let inputImage = inputImage else { return }
         image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let sepiaFilter = CIFilter.sepiaTone()
+        sepiaFilter.inputImage = beginImage
+        sepiaFilter.intensity = 1
+        
+        guard let outputImage = sepiaFilter.outputImage else {
+            return
+        }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            let imageSaver = ImageSaver()
+            imageSaver.writeToPhotoAlbum(image: uiImage)
+        }
+        
+
     }
 }
 
